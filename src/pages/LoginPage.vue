448543729ddfb015d3d972682246a77d583177c5
Alexander <alexsanderesramos@gmail.com>
2025-10-27 19:33:13 -0300

    fear: login add icons

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -10,21 +10,20 @@
             </v-col>
             <v-col class="d-flex align-center justify-center" cols="12" md="6">
                 <div class="pa-4 w-100" style="max-width: 400px;">
-                    <v-form @submit.prevent="" class="d-flex flex-column ga-4" v-model="valid">
+                    <v-form @submit.prevent="handleSubmit" class="d-flex flex-column ga-4" v-model="valid">
                         <div class="text-center mb-6">
                             <h1 class="text-secondary">Login</h1>
                             <p class="text-grey-darken-1">Preencha seus dados para acessar o sistema</p>
                         </div>
 
-                        <v-text-field label="E-mail" variant="outlined" type="email"
+                        <v-text-field v-model="formData.email" label="E-mail" variant="outlined" type="email"
                             :rules="[rules.required, rules.email]"></v-text-field>
 
-                        <v-text-field label="Senha" variant="outlined" :type="typePassword"
+                        <v-text-field v-model="formData.password" label="Senha" variant="outlined" :type="typePassword"
                             :rules="[rules.required, rules.password]" :append-inner-icon="currentIconPassword"
                             @click:append-inner="showPassword"></v-text-field>
 
-                        <v-btn :to="{ name: 'home' }" :disabled="!valid" block class="mb-3" color="secondary"
-                            size="large" type="submit">
+                        <v-btn :disabled="!valid" block class="mb-3" color="secondary" size="large" type="submit">
                             Entrar
                         </v-btn>
 
@@ -44,9 +43,22 @@
 </template>
 
 <script setup>
+import { useNotification } from '@/composables/useNotification'
+import { useRouter } from 'vuetify/lib/composables/router'
+import { useApi } from '@/composables/useApi'
+
+const router = useRouter()
+const notification = useNotification()
+const api = useApi()
+
 const valid = ref(false)
 const currentIconPassword = ref('mdi-eye-off')
 const typePassword = ref('password')
+const formData = ref({
+    email: '',
+    password: ''
+})
+
 const rules = {
     required: value => !!value || 'Campo obrigatório',
     email: value => {
@@ -59,5 +71,23 @@ const rules = {
 function showPassword() {
     typePassword.value = typePassword.value == 'password' ? 'text' : 'password'
     currentIconPassword.value = typePassword.value == 'password' ? 'mdi-eye-off' : 'mdi-eye'
+}
+
+function handleSubmit() {
+    if (!valid.value) {
+        alert('Formulário incompleto')
+    }
+    login()
+}
+
+async function login() {
+    try {
+        const response = await api.post('user/login', { ...formData.value })
+        localStorage.setItem('v-token', response.data.token)
+        notification.success('Login efetuado com sucesso')
+        router.push({ name: 'home' })
+    } catch (error) {
+        notification.error(`Erro ao criar o usuário ${formData.value.name}. Detalhes: ${error?.response?.data?.detail}`, 9000)
+    }
 }
 </script>
