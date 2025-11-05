9fbec67022915524af1091be5c8fef188567409a
Alexander <alexsanderesramos@gmail.com>
2025-11-05 14:42:40 -0300

    feat: dashboard

---
+++ src/components/CardCustom.vue
@@ -0,0 +1,36 @@
+<template>
+    <v-card class="mx-auto my-8" elevation="16" max-width="344" variant="flat">
+        <v-card-item>
+            <v-card-title>
+                {{ title }} - Total: {{ total }}
+            </v-card-title>
+
+            <v-card-subtitle>
+                <v-row>
+                    <v-col class="text-center">
+                        <v-chip color="green" variant="flat">
+                            {{ activeRecordName }}: {{ activeCount }}
+                        </v-chip>
+                    </v-col>
+                    <v-col class="text-center">
+                        <v-chip color="red" variant="flat">
+                            {{ inactiveRecordName }}: {{ inactiveCount }}
+                        </v-chip>
+                    </v-col>
+                </v-row>
+            </v-card-subtitle>
+        </v-card-item>
+    </v-card>
+</template>
+
+<script setup>
+const props = defineProps({
+    title: String,
+    total: Number,
+    activeCount: Number,
+    inactiveCount: Number,
+})
+
+const activeRecordName = computed(() => props?.title.at(-2) === 'o' ? 'Ativos' : 'Ativas')
+const inactiveRecordName = computed(() => props?.title.at(-2) === 'o' ? 'Inativos' : 'Inativas')
+</script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,9 +1,33 @@
-<script setup>
-
-</script>
-
 <template>
     <container-custom>
-        <h1>Home</h1>
+        <div class="d-flex ga-4">
+            <CardCustom title="Categorias" :total="totalCategories" :activeCount="totalActiveCategory"
+                :inactiveCount="totalInactiveCategory" />
+
+            <CardCustom title="Produtos" :total="totalProducts" :activeCount="totalActiveProduct"
+                :inactiveCount="totalInactiveProduct" />
+        </div>
     </container-custom>
 </template>
+
+<script setup>
+import { useCategoryService, useProductService } from '@/services';
+
+const categoryService = useCategoryService()
+const categories = ref([])
+const totalCategories = computed(() => categories.value.length)
+const totalActiveCategory = computed(() => categories.value.filter(item => item.status === 'active').length)
+const totalInactiveCategory = computed(() => categories.value.filter(item => item.status === 'inactive').length)
+
+const productService = useProductService()
+const products = ref([])
+const totalProducts = computed(() => products.value.length)
+const totalActiveProduct = computed(() => products.value.filter(item => item.status === 'active').length)
+const totalInactiveProduct = computed(() => products.value.filter(item => item.status === 'inactive').length)
+
+
+onMounted(async () => {
+    categories.value = await categoryService.getAll()
+    products.value = await productService.getAll()
+})
+</script>
